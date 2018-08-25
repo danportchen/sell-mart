40d091f721b411633bcaf429692e6a2ed4cdb754
danportchen <danport.chen@gmail.com>
2018-08-25 21:07:52 +0800

    檔案整理

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <!-- 頭部 -->
+    <div class="header">
+      header
+    </div>
+
+    <!-- 導航列 -->
+    <div class="nav">
+      nav
+    </div>
+
+    <!-- 主體內容 -->
+    <div class="content">
+      content
+    </div>
+
+
+
+
+
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
